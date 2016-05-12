77974b78cfc5ba1b86c7ea9fdea65fe32ea98de3
Zhongliang Wang <zwang@rebelworks.io>
2016-05-13 00:30:30 +0800

    Add new picture-grid support

--- app/routes/detail.js
+++ app/routes/detail.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  ajax: Ember.inject.service(),
   model(params) {
-    return {
-      id: params.id,
-      url:`https://source.unsplash.com/${params.id}/1600x900`
-    };
-    /*
     return new Ember.RSVP.Promise((resolve)=> {
       fetch(`https://source.unsplash.com/${params.id}/1600x900`).then((response)=> {
         response.blob().then((blobResult)=> {
@@ -18,6 +12,5 @@ export default Ember.Route.extend({
         });
       });
     });
-   */
   }
 });
