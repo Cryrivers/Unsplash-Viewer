0a04779125babc4b371110c9a6c8960ffe8e8c87
Wang Zhongliang <wangz@garena.com>
2016-05-12 19:33:08 +0800

    Add styles for iOS

--- app/routes/detail.js
+++ app/routes/detail.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
   model(params) {
+    return {
+      id: params.id,
+      url:`https://source.unsplash.com/${params.id}/1600x900`
+    };
+    /*
     return new Ember.RSVP.Promise((resolve)=> {
       fetch(`https://source.unsplash.com/${params.id}/1600x900`).then((response)=> {
         response.blob().then((blobResult)=> {
@@ -13,5 +18,6 @@ export default Ember.Route.extend({
         });
       });
     });
+   */
   }
 });
