7b81c5715a87796d47fe2a7f7dcb7ca3d7c672eb
Wang Zhongliang <wangz@garena.com>
2016-05-13 12:24:39 +0800

    Update transition mapping, add support for Cordova

--- app/transitions.js
+++ app/transitions.js
@@ -1,12 +1,39 @@
-export default function() {
-  this.transition(
-    this.fromRoute('index'),
-    this.toRoute('detail'),
-    this.useAndReverse('explode', {
-      matchBy: 'data-picture-id',
-      use: ['fly-to', {duration: 300}]
-    }, {
-      use: ['fade', {duration: 300}]
-    })
-  );
+import platform from './utils/platform';
+
+let transitionFunction;
+
+if (platform.isCordova()) {
+  transitionFunction = function() {
+    this.transition(
+      this.fromRoute('index'),
+      this.toRoute('detail'),
+      this.use('explode', {
+        matchBy: 'data-picture-id',
+        use: ['fly-to', {duration: 300}]
+      }, {
+        use: ['toLeft', {duration: 300}]
+      }),
+      this.reverse('explode', {
+        matchBy: 'data-picture-id',
+        use: ['fly-to', {duration: 300}]
+      }, {
+        use: ['toRight', {duration: 300}]
+      })
+    );
+  };
+} else {
+  transitionFunction = function() {
+    this.transition(
+      this.fromRoute('index'),
+      this.toRoute('detail'),
+      this.useAndReverse('explode', {
+        matchBy: 'data-picture-id',
+        use: ['fly-to', {duration: 300}]
+      }, {
+        use: ['fade', {duration: 300}]
+      })
+    );
+  };
 }
+
+export default transitionFunction;
